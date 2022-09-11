9b6f06cf0e334f8740e392ad629d616b8be878bd
AkramAbuOwaimer <Akram.AbuOwaimer@gmail.com>
2022-09-11 23:19:36 +0300

    init

---
+++ main.ino
@@ -0,0 +1,42 @@
+// if 
+// while
+// for
+// switch 
+// function 
+
+// string, char, byte, int, float, double, bool, ...
+
+// * array
+// * object of class
+
+// ! union 
+// ! struct
+// ! class
+
+// ? basics of electronics #
+// ? - types of electrical lines #
+// ? - types of signals #
+// ? - basic electronic components
+// ? - basic circuitry 
+
+// ? basics of codeing 
+// ? - coding essentials
+// ? - arduino programming basics 
+// ? - how to communicate with arduino
+
+// ? arduino programming
+// ? - digital input/output
+// ? - analog  input/output
+// ? - hardware interrupt
+// ? - communications 
+// ? - projects
+
+void setup()
+{
+    
+}
+
+void loop()
+{
+
+}
